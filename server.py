f334b6f4e3980ff2560b9b695aaa362a5fa7fced
Fawaris <138193177+Fawaris@users.noreply.github.com>
2024-07-09 02:18:36 +0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Переменная для хранения состояния вибрации
+vibration_state = {"vibrate": False}
+
+@app.route('/send_vibration', methods=['POST'])
+def send_vibration():
+    global vibration_state
+    vibration_state["vibrate"] = True
+    return '', 204
+
+@app.route('/receive_vibration', methods=['GET'])
+def check_vibration():
+    global vibration_state
+    response = jsonify(vibration_state)
+    # Сбрасываем состояние вибрации после получения
+    vibration_state["vibrate"] = False
+    return response
+
+if __name__ == '__main__':
+    app.run()
